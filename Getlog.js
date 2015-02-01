0839265e55e7379d53099f29d10b521f05dab60b
Chuntao <jichuntao@hotmail.com>
2015-02-01 22:12:19 +0800

    ok

--- Getlog.js
+++ Getlog.js
@@ -1,117 +1,278 @@
 /**
  * Created with JetBrains WebStorm.
  * User: jichuntao
- * Date: 13-11-4
- * Time: 下午7:04
+ * Date: 13-9-13
+ * Time: 下午3:53
  * To change this template use File | Settings | File Templates.
  */
-
+var ndir = require('ndir');
 var fs = require('fs');
-var lineReader = require('line-reader');
-var util = require('util');
-var zlib = require('zlib');
 var query = require("querystring");
-var path='/mnt/farmweblog3/netError/';
-//var path='./xxx/';
-var ret={};
-var retArr=[];
-var retRetry={};
 
-var datedir='2013_11_3';
-var langdir='am';
+//var dir = './xxx/2015_1_30/farm-tw';
+var dir = '/mnt/farmSocketLog/';
+var i = 0;
+var debug_args = arguments[0];
+var files ;
 
-
+var users=[];
+var user;
 function exe(req, res, rf, data) {
+    console.log(data);
     var qu = query.parse(data);
     datedir = qu['date'];
     langdir = qu['lang'];
-    ret={};
-    retArr=[];
-    retRetry={};
-    var filename=path+datedir+'/'+langdir+'/'+datedir+'_netError_'+langdir+'.log';
-    openfile(filename,function(data){
-        res.write(data, "binary");
+    dir =  '/mnt/farmSocketLog/'+datedir+'/farm-'+langdir;
+    files = fs.readdirSync(dir+'/');
+    files = ignoreArr(files);
+    user=null;
+    users=[];
+    i=0;
+    start(function(){
+        var ret={};
+        var jj=0;
+        ret.allNum=users.length;
+        ret.errorNum=0;
+        ret.loginNum=0;
+        ret.closeNum=0;
+        var outTimeArr=[];
+        var inTimeArr=[];
+        var outPrecentArr=[];
+        for(jj=0;jj<users.length;jj++)
+        {
+
+            var u=users[jj];
+
+            var loadingTime=Math.round(u.loadingTime/1000);
+            if(u.status=='error'){
+                ret.errorNum++;
+                if(!outPrecentArr[u.percent]){
+                    outPrecentArr[u.percent]=0;
+                }
+                outPrecentArr[u.percent]++;
+            }
+            else if(u.status=='login'){
+                ret.loginNum++;
+                if(!inTimeArr[loadingTime]){
+                    inTimeArr[loadingTime]=0;
+                }
+                inTimeArr[loadingTime]++;
+            }
+            else if(u.status=='close'){
+                ret.closeNum++;
+                if(!outTimeArr[loadingTime]){
+                    outTimeArr[loadingTime]=0;
+                }
+                outTimeArr[loadingTime]++;
+                if(!outPrecentArr[u.percent]){
+                    outPrecentArr[u.percent]=0;
+                }
+                outPrecentArr[u.percent]++;
+            }else{
+                if(u.percent==100){
+                    ret.loginNum++;
+                    if(!inTimeArr[loadingTime]){
+                        inTimeArr[loadingTime]=0;
+                    }
+                    inTimeArr[loadingTime]++;
+                }else {
+                    ret.closeNum++;
+                    if(!outTimeArr[loadingTime]){
+                        outTimeArr[loadingTime]=0;
+                    }
+                    outTimeArr[loadingTime]++;
+                    if(!outPrecentArr[u.percent]){
+                        outPrecentArr[u.percent]=0;
+                    }
+                    outPrecentArr[u.percent]++;
+                }
+            }
+        }
+
+        
+        res.write('<p> 总的统计人数: '+ret.allNum+'</p>');
+        res.write('<p> 成功进入人数: '+ret.loginNum+'</p>');
+        res.write('<p> 离开人数: '+ret.closeNum+'</p>');
+        res.write('<p> 加载报错人数: '+ret.errorNum+'</p>');
+        res.write('</br>');
+        var sum=0;
+        for(jj=0;jj<outTimeArr.length;jj++){
+            if(outTimeArr[jj]){
+                sum+=outTimeArr[jj];
+            }
+            //var sp='&nbsp;&nbsp;&nbsp;&nbsp;('+Math.round((sum/outTimeArr.length)*100)+'%)';
+            if(jj==5){
+                 res.write('<p> 5秒离开人数: '+sum+'</p>');
+            }else if(jj==10){
+                 res.write('<p> 10秒离开人数: '+sum+'</p>');
+            }else if(jj==15){
+                 res.write('<p> 15秒离开人数: '+sum+'</p>');
+            }else if(jj==20){
+                 res.write('<p> 20秒离开人数: '+sum+'</p>');
+            }else if(jj==30){
+                 res.write('<p> 30秒离开人数: '+sum+'</p>');
+            }else if(jj==40){
+                 res.write('<p> 40秒离开人数: '+sum+'</p>');
+            }else if(jj==60){
+                 res.write('<p> 60秒离开人数: '+sum+'</p>');
+            }else if(jj==80){
+                 res.write('<p> 80秒离开人数: '+sum+'</p>');
+            }else if(jj==(outTimeArr.length-1)){
+                 res.write('<p> 80秒以上离开人数: '+sum+'</p>');
+            }
+        }
+        res.write('</br>');
+        sum=0;
+        for(jj=0;jj<outPrecentArr.length;jj++){
+            if(outPrecentArr[jj]){
+                sum+=outPrecentArr[jj];
+            }
+            if(jj==10){
+                 res.write('<p> 加载到10%离开人数: '+sum+'</p>');
+            }else if(jj==20){
+                 res.write('<p> 加载到20%离开人数: '+sum+'</p>');
+            }else if(jj==50){
+                 res.write('<p> 加载到50%离开人数: '+sum+'</p>');
+            }else if(jj==80){
+                 res.write('<p> 加载到80%离开人数: '+sum+'</p>');
+            }else if(jj==90){
+                 res.write('<p> 加载到90%离开人数: '+sum+'</p>');
+            }else if(jj==(outPrecentArr.length-1)){
+                 res.write('<p> 加载到90%以上离开人数: '+sum+'</p>');
+            }
+        }
+        res.write('</br>');
+        sum=0;
+        for(jj=0;jj<inTimeArr.length;jj++){
+            if(inTimeArr[jj]){
+                sum+=inTimeArr[jj];
+            }
+            if(jj==5){
+                 res.write('<p> 5秒内进入人数: '+sum+'</p>');
+            }else if(jj==10){
+                 res.write('<p> 10秒内进入人数: '+sum+'</p>');
+            }else if(jj==15){
+                 res.write('<p> 15秒内进入人数: '+sum+'</p>');
+            }else if(jj==20){
+                 res.write('<p> 20秒内进入人数: '+sum+'</p>');
+            }else if(jj==30){
+                 res.write('<p> 30秒内进入人数: '+sum+'</p>');
+            }else if(jj==40){
+                 res.write('<p> 40秒内进入人数: '+sum+'</p>');
+            }else if(jj==60){
+                 res.write('<p> 60秒内进入人数: '+sum+'</p>');
+            }else if(jj==80){
+                 res.write('<p> 80秒内进入人数: '+sum+'</p>');
+            }else if(jj==100){
+                 res.write('<p> 100秒内进入人数: '+sum+'</p>');
+            }else if(jj==120){
+                 res.write('<p> 120秒内进入人数: '+sum+'</p>');
+            }else if(jj==180){
+                 res.write('<p> 180秒内进入人数: '+sum+'</p>');
+            }else if(jj==(inTimeArr.length-1)){
+                 res.write('<p> 180秒以上进入人数: '+sum+'</p>');
+            }
+        }
         res.end();
     });
+   
 }
 
-//开始打开文件
-function openfile(filename,callback) {
-    lineReader.eachLine(filename, function (line, last, cb) {
-        exec(line, function () {
-            if (last) {
-                var obj={};
-                obj.data=retArr;
-            obj.ret=ret;
-            obj.retry=retRetry;
-            zlib.deflate(JSON.stringify(obj), function(err, buffer) {
-                callback(buffer);
-                cb(false);
-            });
+
+
+
+//开始执行
+function start(cb) {
+    var path = dir +'/'+ files[i] ;
+    i++;
+    user={};
+    //console.log("Start read:" + path);
+    openfile(path,callback);
+    function callback(){
+        if(user && user.uid){
+           users.push(user);
+        }
+        if (i == files.length){
+            cb();
             return;
         }
-            cb();
-        });
-    });
+        path = dir +'/'+ files[i] ;
+        i++;
+        user={};
+        //console.log("Start read:" + path);
+        openfile(path,callback);
+    }
 }
 
-function exec(strs, cb) {
+function exec(strs) {
     try {
+        strs = strs.replace(/\'/g, '"');
         var data = strs.substr(strs.indexOf('{'));
         var key = strs.substring(0, strs.indexOf('{') - 1);
         var obj = JSON.parse(data);
         var logtime = key.split('-')[0];
-        var lang = key.split('-')[2];
-        var uid = obj.uid;
-        var date=new Date(logtime*1000);
-        var retryStates=false;
-        if(obj.retryStates){
-            retryStates=obj.retryStates;
+        var action = obj.action;
+        if(action == 'login'){
+            user.uid=obj.uid;
         }
-        if(obj.errarr.length==0){
-            pushObj('_0');
-            pushObj_retry('_0',retryStates);
-            retArr.push({time:logtime,type:0,retryStates:retryStates});
-        }else{
-            if(obj.errarr[0].msg.description=='HTTP: Status 503'){
-                pushObj('_1');
-                pushObj_retry('_1',retryStates);
-                retArr.push({time:logtime,type:1,retryStates:retryStates});
-            }else if(obj.errarr[0].msg.description=='HTTP: Status 502'){
-                pushObj('_2');
-                pushObj_retry('_2',retryStates);
-                retArr.push({time:logtime,type:2,retryStates:retryStates});
-            }else if(obj.errarr[0].msg.description=='HTTP: Failed'){
-                pushObj('_3');
-                pushObj_retry('_3',retryStates);
-                retArr.push({time:logtime,type:3,retryStates:retryStates});
-            }else{
-                pushObj_retry('_4',retryStates);
-                retArr.push({time:logtime,type:4,retryStates:retryStates});
-                pushObj('_4');
+        else if(action=='loading')
+        {
+            if(!user.percent){
+                user.percent=obj.percent;
+                user.loadingTime=obj.time;
+            }
+            if(obj.percent>user.percent){
+                user.percent=obj.percent;
+                user.loadingTime=obj.time;
             }
         }
-        cb();
+        else if(action=='logout')
+        {
+            user.loadingTime=obj.time;
+            user.status='login';
+        }
+        else if(action=='load_error')
+        {
+            user.loadingTime=obj.time;
+            user.status='error';
+        }
+        else if(action=='close' && user.status!='error')
+        {
+            user.loadingTime=obj.time;
+            user.status='close';
+        }
     }
-    catch (e){
-        console.log('Err:0'+e);
-        cb();
+    catch (err) {
+        console.log(err + " # " + strs);
     }
 }
 
-    function pushObj(key){
-        if(!ret[key]){
-            ret[key]=0;
-        }
-        ret[key]++;
+
+//开始打开文件
+function openfile(filename,cb) {
+    var file = ndir.createLineReader(filename);
+    file.on('line', function (line) {
+        exec(line.toString());
+    });
+    file.on('end', function () {
+       // console.log('over:' + filename);
+        cb();
+    });
+    file.on('error', function (err) {
+        console.log('fileErr:' + err);
+        //cb();
+    });
 }
-function pushObj_retry(key,retryStates){
-    if(!retryStates){
-        return;
+
+//过滤数组
+function ignoreArr(arr) {
+    var ret = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == '.DS_Store') {
+            continue;
+        }
+        ret.push(arr[i]);
     }
-    if(!retRetry[key]){
-        retRetry[key]=0;
-    }
-    retRetry[key]++;
+    return ret;
 }
 exports.exe=exe;
